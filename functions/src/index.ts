976f06ca824069b248b412eee69ba01c307ce07a
ANDREYDEN <andrey2850@gmail.com>
2021-07-23 23:50:25 -0400

    Set up local functions

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -2,13 +2,12 @@ import * as functions from "firebase-functions";
 import * as vision from "@google-cloud/vision";
 import * as admin from 'firebase-admin';
 import 'firebase-functions';
-admin.initializeApp();
 
-// const serviceAccount = require('./serviceAccount.json');
+import serviceAccount from '../serviceAccount.json';
+import { ServiceAccount } from "firebase-admin";
 
-export const helloWorld = functions.https.onRequest((request, response) => {
-  functions.logger.info("Hello logs!", {structuredData: true});
-  response.send("Hello from Firebase!");
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount as ServiceAccount)
 });
 
 export const quickstart = functions.https.onRequest(async (request, response) => {
@@ -18,14 +17,5 @@ export const quickstart = functions.https.onRequest(async (request, response) =>
   // Performs label detection on the image file
   const [result] = await client.textDetection("https://firebasestorage.googleapis.com/v0/b/statera-0.appspot.com/o/receipt.jpg?alt=media&token=ae9766ca-063d-4aad-a510-d168b9dde125");
   const labels = result.textAnnotations;
-  console.log("Text:");
   response.send(labels?.map((label) => label.description));
 });
-
-
-// const adminConfig = JSON.parse(process.env.FIREBASE_CONFIG ?? '');
-// adminConfig.credential = admin.credential.cert(serviceAccount);
-// admin.initializeApp(adminConfig);
-
-
-
