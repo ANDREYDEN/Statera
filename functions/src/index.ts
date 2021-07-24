04c7909f8985afd1492492f08f0b3fb43277e6a4
ANDREYDEN <andrey2850@gmail.com>
2021-07-24 00:23:11 -0400

    Add parameter to the receipt analyzer

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,21 +1,20 @@
 import * as functions from "firebase-functions";
 import * as vision from "@google-cloud/vision";
-import * as admin from 'firebase-admin';
-import 'firebase-functions';
+import "firebase-functions";
 
-import serviceAccount from '../serviceAccount.json';
-import { ServiceAccount } from "firebase-admin";
+export const quickstart = functions.https.onRequest(
+  async (request, response) => {
+    if (!request.query.receiptUrl) {
+      response.status(400).send("Parameter receiptUrl is required");
+    }
+    // Creates a client
+    const client = new vision.ImageAnnotatorClient();
 
-admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount as ServiceAccount)
-});
-
-export const quickstart = functions.https.onRequest(async (request, response) => {
-  // Creates a client
-  const client = new vision.ImageAnnotatorClient();
-
-  // Performs label detection on the image file
-  const [result] = await client.textDetection("https://firebasestorage.googleapis.com/v0/b/statera-0.appspot.com/o/receipt.jpg?alt=media&token=ae9766ca-063d-4aad-a510-d168b9dde125");
-  const labels = result.textAnnotations;
-  response.send(labels?.map((label) => label.description));
-});
+    // Performs label detection on the image file
+    const [result] = await client.textDetection(
+      request.query.receiptUrl as string
+    );
+    const labels = result.textAnnotations;
+    response.send(labels?.map((label) => label.description));
+  }
+);
