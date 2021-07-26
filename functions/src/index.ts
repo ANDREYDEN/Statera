b0b512686a39cb22bc7462d50fcbef0417a56e5f
ques-vehikl <61066147+ques-vehikl@users.noreply.github.com>
2021-07-26 17:07:32 -0400

    wip normalize everything

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,17 +1,19 @@
+/* eslint-disable require-jsdoc */
 import * as functions from "firebase-functions";
 import * as vision from "@google-cloud/vision";
 import "firebase-functions";
+import {mergePrices, normalize} from "./normalizers";
 
 export const getReceiptDataTest = functions.https.onRequest(
-  async (request, response) => {
-    const receiptUrl = request.query.receiptUrl;
-    if (!receiptUrl) {
-      response.status(400).send("Parameter receiptUrl is required");
-    }
+    async (request, response) => {
+      const receiptUrl = request.query.receiptUrl;
+      if (!receiptUrl) {
+        response.status(400).send("Parameter receiptUrl is required");
+      }
 
-    const result = await analyzeReceipt(receiptUrl as string);
-    response.send(result);
-  }
+      const result = await analyzeReceipt(receiptUrl as string);
+      response.send(result);
+    }
 );
 
 export const getReceiptData = functions.https.onCall(async (data, context) => {
@@ -22,18 +24,18 @@ export const getReceiptData = functions.https.onCall(async (data, context) => {
   return analyzeReceipt(data.receiptUrl);
 });
 
-async function analyzeReceipt(receiptUrl: string): Promise<any> {
+async function analyzeReceipt(receiptUrl: string): Promise<{ [k: string]: any[]; }> {
   const client = new vision.ImageAnnotatorClient();
 
   const [result] = await client.textDetection(receiptUrl);
   const labels = result.textAnnotations ?? [];
-  
+
   const lines: Map<number, any[]> = new Map();
 
-  labels.forEach(label => {
+  labels.forEach((label) => {
     const THRESHOLD = 12;
     const labelHeight = baselineHeight(label);
-    
+
     let similarHeight = labelHeight;
     for (const height of lines.keys()) {
       if (Math.abs(labelHeight - height) < THRESHOLD) {
@@ -44,9 +46,21 @@ async function analyzeReceipt(receiptUrl: string): Promise<any> {
 
     const previousValue = lines.has(similarHeight) ? lines.get(similarHeight)! : [];
     lines.set(similarHeight, [...previousValue, label.description]);
-  })
-   
-  return Object.fromEntries(lines.entries());
+  });
+
+
+  const data = Object.fromEntries(lines.entries());
+
+  console.log("before loop");
+  console.log("Normalized: ", mergePrices(data));
+
+  for (const key in data) {
+    const returnValue = normalize(data[key]);
+    console.log("Normalized: ", returnValue);
+  }
+
+
+  return data;
 }
 
 function baselineHeight(label: any): number {
--- functions/src/normalizers.ts
+++ functions/src/normalizers.ts
@@ -1,19 +1,29 @@
-const CODE_REGEX = /\d{11, 13}/;
+/* eslint-disable require-jsdoc */
+const CODE_REGEX = /\d{11,13}/;
 
-export function normalize(row: string[] ) {
-  let name: string | null = null;
-  let code: number | null = null;
-  let value: number | null = null;
+export function normalize(row: string[]) {
+  const name: string | null = null;
+  let code: string | null = null;
+  const value: number | null = null;
 
-  row.forEach(element => {
+  row.forEach((element) => {
     const codeMatcher = element.match(CODE_REGEX);
     if (codeMatcher) {
-      code = +codeMatcher[0];
-      element.replace(codeMatcher[0], '');
+      code = codeMatcher[0];
+      element.replace(codeMatcher[0], "");
     }
-
-    
   });
 
-  return { name, code, value };
+  return {name, code, value};
+}
+
+export function mergePrices(rows: { [height: string] : any[] }): { [height: string] : any[] } {
+  const result: { [height: string] : any[] } = {};
+  Object.entries(rows).forEach(([height, row], i) => {
+    if (row[1] === 'H') {
+      result[i-1] = [...result[i-1], ...row];
+    }
+  });
+
+  return result;
 }
