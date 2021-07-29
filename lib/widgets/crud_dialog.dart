22c6fbbff90d9a1001556bfc5b3a62ec3f530486
ANDREYDEN <andrey2850@gmail.com>
2021-07-29 16:13:53 -0400

    Add focusing logic

--- lib/widgets/crud_dialog.dart
+++ lib/widgets/crud_dialog.dart
@@ -6,12 +6,11 @@ class FieldData {
   late TextEditingController controller;
   TextInputType inputType;
 
-  FieldData({
-    required this.id,
-    required this.label,
-    TextEditingController? controller,
-    this.inputType = TextInputType.name
-  }) {
+  FieldData(
+      {required this.id,
+      required this.label,
+      TextEditingController? controller,
+      this.inputType = TextInputType.name}) {
     this.controller = controller ?? TextEditingController();
   }
 }
@@ -49,49 +48,57 @@ class _CRUDDialogState extends State<CRUDDialog> {
   Widget build(BuildContext context) {
     return AlertDialog(
       title: Text(widget.title),
-      content: Column(
-        children: widget.fields
-            .map(
-              (field) => TextField(
-                controller: field.controller,
-                keyboardType: field.inputType,
-                decoration: InputDecoration(
-                  labelText: field.label,
-                  errorText: field.controller.text.isEmpty && this._dirty
-                      ? "Can't be empty"
-                      : null,
-                ),
-              ),
-            )
-            .toList(),
-      ),
-      actions: [
-        ElevatedButton(
-          onPressed: () async {
-            if (widget.fields.any((field) => field.controller.text.isEmpty)) {
-              setState(() {
-                this._dirty = true;
-              });
-              return;
-            }
-
-            await widget.onSubmit(
-              Map.fromEntries(widget.fields.map(
-                (field) => MapEntry(
-                  field.id,
-                  field.controller.text,
-                ),
-              )),
-            );
-
-            widget.fields.forEach((field) {
-              field.controller.clear();
-            });
-            Navigator.of(context).pop();
-          },
-          child: Text("Save"),
-        )
-      ],
+      content: Column(children: [...textFields]),
+      actions: [ElevatedButton(onPressed: submit, child: Text("Save"))],
     );
+  }
+
+  Iterable<Widget> get textFields sync* {
+    var focusNodes = widget.fields.map((field) => FocusNode(debugLabel: field.id)).toList();
+    for (var i = 0; i < widget.fields.length; i++) {
+      var field = widget.fields[i];
+      yield TextField(
+        autofocus: i == 0,
+        focusNode: focusNodes[i],
+        controller: field.controller,
+        keyboardType: field.inputType,
+        decoration: InputDecoration(
+          labelText: field.label,
+          errorText: field.controller.text.isEmpty && this._dirty
+              ? "Can't be empty"
+              : null,
+        ),
+        onSubmitted: (_) {
+          if (i == widget.fields.length - 1) {
+            submit();
+          } else {
+            focusNodes[i+1].requestFocus();
+          }
+        }
+      );
+    }
+  }
+
+  submit() async {
+    if (widget.fields.any((field) => field.controller.text.isEmpty)) {
+      setState(() {
+        this._dirty = true;
+      });
+      return;
+    }
+
+    await widget.onSubmit(
+      Map.fromEntries(widget.fields.map(
+        (field) => MapEntry(
+          field.id,
+          field.controller.text,
+        ),
+      )),
+    );
+
+    widget.fields.forEach((field) {
+      field.controller.clear();
+    });
+    Navigator.of(context).pop();
   }
 }
