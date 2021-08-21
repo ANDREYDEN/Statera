28dca9b61f08933090e44d579ec526226d06b28c
ANDREYDEN <andrey2850@gmail.com>
2021-08-21 17:11:35 -0400

    Organization

---
+++ lib/utils/formatters.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+
+class CommaReplacerTextInputFormatter extends TextInputFormatter {
+  @override
+  TextEditingValue formatEditUpdate(
+    TextEditingValue oldValue,
+    TextEditingValue newValue,
+  ) {
+    String truncated = newValue.text;
+    TextSelection newSelection = newValue.selection;
+
+    truncated = newValue.text.replaceAll(RegExp(','), '.');
+    return TextEditingValue(
+      text: truncated,
+      selection: newSelection,
+    );
+  }
+}
