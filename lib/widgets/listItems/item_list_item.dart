fd55ef91db593b89b2e8f5d75bfdd7256df56927
ANDREYDEN <andrey2850@gmail.com>
2021-09-16 01:40:25 -0400

    Add partition progress bar

--- lib/widgets/listItems/item_list_item.dart
+++ lib/widgets/listItems/item_list_item.dart
@@ -38,52 +38,96 @@ class ItemListItem extends StatelessWidget {
           ),
           SizedBox(width: 10),
           VerticalDivider(thickness: 1, indent: 10, endIndent: 10),
-          Row(
-            mainAxisSize: MainAxisSize.min,
-            children: [
-              ElevatedButton(
-                onPressed: () =>
-                    this.onChangePartition(authVm.getItemParts(item) - 1),
-                style: ElevatedButton.styleFrom(
-                  shape: CircleBorder(),
-                  primary: !authVm.hasDecidedOn(item)
-                      ? Colors.grey[300]
-                      : authVm.hasDenied(item)
-                          ? Colors.red[400]
-                          : Colors.grey[500],
-                  padding: EdgeInsets.all(0),
+          IntrinsicWidth(
+            child: Column(
+              children: [
+                Row(
+                  mainAxisSize: MainAxisSize.min,
+                  children: [
+                    ElevatedButton(
+                      onPressed: () =>
+                          this.onChangePartition(authVm.getItemParts(item) - 1),
+                      style: ElevatedButton.styleFrom(
+                        shape: CircleBorder(),
+                        primary: !authVm.hasDecidedOn(item)
+                            ? Colors.grey[300]
+                            : authVm.hasDenied(item)
+                                ? Colors.red[400]
+                                : Colors.grey[500],
+                        padding: EdgeInsets.all(0),
+                      ),
+                      child: Icon(
+                        !authVm.hasDecidedOn(item)
+                            ? Icons.close
+                            : authVm.hasDenied(item)
+                                ? Icons.close
+                                : Icons.remove,
+                        color: Colors.white,
+                      ),
+                    ),
+                    Text(
+                        "${authVm.hasDecidedOn(item) ? authVm.getItemParts(item) : '-'}/${item.partition}"),
+                    ElevatedButton(
+                      onPressed: () =>
+                          this.onChangePartition(authVm.getItemParts(item) + 1),
+                      style: ElevatedButton.styleFrom(
+                        shape: CircleBorder(),
+                        primary: !authVm.hasDecidedOn(item)
+                            ? Colors.grey[300]
+                            : item.undefinedParts == 0 &&
+                                    authVm.hasConfirmed(item)
+                                ? Colors.green[400]
+                                : Colors.grey[500],
+                      ),
+                      child: Icon(
+                        !authVm.hasDecidedOn(item)
+                            ? Icons.check
+                            : item.undefinedParts == 0 &&
+                                    authVm.hasConfirmed(item)
+                                ? Icons.check
+                                : Icons.add,
+                        color: Colors.white,
+                      ),
+                    ),
+                  ],
                 ),
-                child: Icon(
-                  !authVm.hasDecidedOn(item)
-                      ? Icons.close
-                      : authVm.hasDenied(item)
-                          ? Icons.close
-                          : Icons.remove,
-                  color: Colors.white,
+                Container(
+                  padding: EdgeInsets.symmetric(horizontal: 8),
+                  height: 10,
+                  child: Row(
+                    children: [
+                      Flexible(
+                        flex: item.confirmedParts,
+                        child: Container(
+                          decoration: BoxDecoration(
+                            borderRadius: BorderRadius.only(
+                              bottomLeft: Radius.circular(10),
+                              topLeft: Radius.circular(10),
+                              bottomRight: Radius.circular(item.undefinedParts == 0 ? 10 : 0),
+                              topRight: Radius.circular(item.undefinedParts == 0 ? 10 : 0),
+                            ),
+                            color: Colors.green[400],
+                          ),
+                        ),
+                      ),
+                      Flexible(
+                        flex: item.undefinedParts,
+                        child: Container(decoration: BoxDecoration(
+                            borderRadius: BorderRadius.only(
+                              bottomRight: Radius.circular(10),
+                              topRight: Radius.circular(10),
+                              bottomLeft: Radius.circular(item.confirmedParts == 0 ? 10 : 0),
+                              topLeft: Radius.circular(item.confirmedParts == 0 ? 10 : 0),
+                            ),
+                            color: Colors.grey[300],
+                          ),),
+                      )
+                    ],
+                  ),
                 ),
-              ),
-              Text("${authVm.hasDecidedOn(item) ? authVm.getItemParts(item) : '-'}/${item.partition}"),
-              ElevatedButton(
-                onPressed: () =>
-                    this.onChangePartition(authVm.getItemParts(item) + 1),
-                style: ElevatedButton.styleFrom(
-                  shape: CircleBorder(),
-                  primary: !authVm.hasDecidedOn(item)
-                      ? Colors.grey[300]
-                      : item.undefinedParts == 0
-                          ? Colors.green[400]
-                          : Colors.grey[500],
-                ),
-                child: Icon(
-                  !authVm.hasDecidedOn(item)
-                      ? Icons.check
-                      : item.undefinedParts == 0
-                          ? Icons.check
-                          : Icons.add,
-                  color: Colors.white,
-                ),
-              ),
-            ],
+                SizedBox(height: 10),
+              ],
+            ),
           )
         ],
       ),
