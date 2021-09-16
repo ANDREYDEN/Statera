36263785459f0ad17cd2472d37035b5e0d6a14ad
ANDREYDEN <andrey2850@gmail.com>
2021-09-16 17:24:08 -0400

    Organize tests

--- test/lib/item_test.dart
+++ test/lib/item_test.dart
@@ -4,8 +4,9 @@ import 'package:statera/models/item.dart';
 
 void main() {
   group('Item', () {
+    late Item item = Item(name: "foo", value: 145.0);
+
     test('gets the number of assignees that confirmed the item', () {
-      var item = Item(name: "foo", value: 145);
       var assignee1 = AssigneeDecision(uid: '1');
       var assignee2 = AssigneeDecision(uid: '2', parts: 1);
 
@@ -15,7 +16,6 @@ void main() {
     });
 
     test("is incomplete when at leat one assignee hasn't made a decision", () {
-      var item = Item(name: "foo", value: 145);
       var assignee1 = AssigneeDecision(uid: '1');
       var assignee2 = AssigneeDecision(uid: '2');
 
@@ -25,7 +25,6 @@ void main() {
     });
 
     test('is completed when all assignees made a decision', () {
-      var item = Item(name: "foo", value: 145);
       var assignee1 = AssigneeDecision(uid: '1', parts: 1);
       var assignee2 = AssigneeDecision(uid: '2', parts: 0);
 
@@ -35,123 +34,122 @@ void main() {
     });
 
     group('not partitioned', () {
-      [
-        {
-          "decisions": [
-            AssigneeDecision(uid: '1', parts: 1),
-            AssigneeDecision(uid: '2', parts: 0),
-            AssigneeDecision(uid: '3', parts: 1),
-          ],
-          "value": 145.0,
-          "expected": 72.5
-        },
-        {
-          "decisions": [
-            AssigneeDecision(uid: '1', parts: 1),
-            AssigneeDecision(uid: '2'),
-            AssigneeDecision(uid: '3'),
-          ],
-          "value": 145.0,
-          "expected": 145.0,
-        },
-        {
-          "decisions": [
-            AssigneeDecision(uid: '1', parts: 0),
-            AssigneeDecision(uid: '2', parts: 0),
-            AssigneeDecision(uid: '3', parts: 0),
-          ],
-          "value": 145.0,
-          "expected": 0.0,
-        },
-        {
-          "decisions": [
-            AssigneeDecision(uid: '1'),
-            AssigneeDecision(uid: '2', parts: 0),
-            AssigneeDecision(uid: '3', parts: 0),
-          ],
-          "value": 145.0,
-          "expected": 0.0,
-        },
-        {
-          "decisions": [
-            AssigneeDecision(uid: '1', parts: 0),
-            AssigneeDecision(uid: '2', parts: 1),
-            AssigneeDecision(uid: '3'),
-          ],
-          "value": 145.0,
-          "expected": 0.0,
-        }
-      ].forEach((testData) {
-        test('gets the shared value', () {
-          var item = Item(name: "foo", value: testData["value"] as double);
+      var item = Item(name: "foo", value: 145.0);
 
-          item.assignees = testData["decisions"] as List<AssigneeDecision>;
+      createSimpleSharedValueTest(
+          {condition, partsList, value, expectedValue}) {
+        createSharedValueTest(
+          item,
+          condition: condition,
+          partsList: partsList,
+          value: value,
+          expectedValue: expectedValue,
+        );
+      }
 
-          expect(item.getSharedValueFor('1'), testData["expected"] as double);
-        });
-      });
+      createSimpleSharedValueTest(
+        condition: "accepted together with someone else",
+        partsList: [1, 0, 1],
+        value: item.value,
+        expectedValue: item.value / 2,
+      );
+
+      createSimpleSharedValueTest(
+        condition: "accepted and everyone else is undefined",
+        partsList: [1, null, null],
+        value: item.value,
+        expectedValue: item.value,
+      );
+
+      createSimpleSharedValueTest(
+        condition: "everybody denied",
+        partsList: [0, 0, 0],
+        value: item.value,
+        expectedValue: 0.0,
+      );
+
+      createSimpleSharedValueTest(
+        condition: "undefined and everybody else denied",
+        partsList: [null, 0, 0],
+        value: item.value,
+        expectedValue: 0.0,
+      );
+
+      createSimpleSharedValueTest(
+        condition: "denied but others did something else",
+        partsList: [0, 0, 0],
+        value: item.value,
+        expectedValue: 0.0,
+      );
     });
 
     group('partitioned', () {
-      [
-        {
-          "decisions": [
-            AssigneeDecision(uid: '1', parts: 1),
-            AssigneeDecision(uid: '2', parts: 1),
-            AssigneeDecision(uid: '3', parts: 1),
-          ],
-          "value": 145.0,
-          "expected": 145.0 / 3
-        },
-        {
-          "decisions": [
-            AssigneeDecision(uid: '1', parts: 2),
-            AssigneeDecision(uid: '2'),
-            AssigneeDecision(uid: '3'),
-          ],
-          "value": 145.0,
-          "expected": 145.0 * 2 / 3,
-        },
-        {
-          "decisions": [
-            AssigneeDecision(uid: '1', parts: 3),
-            AssigneeDecision(uid: '2', parts: 0),
-            AssigneeDecision(uid: '3', parts: 0),
-          ],
-          "value": 145.0,
-          "expected": 145.0,
-        },
-        {
-          "decisions": [
-            AssigneeDecision(uid: '1', parts: 0),
-            AssigneeDecision(uid: '2', parts: 0),
-            AssigneeDecision(uid: '3', parts: 0),
-          ],
-          "value": 145.0,
-          "expected": 0.0,
-        },
-        {
-          "decisions": [
-            AssigneeDecision(uid: '1'),
-            AssigneeDecision(uid: '2', parts: 0),
-            AssigneeDecision(uid: '3', parts: 2),
-          ],
-          "value": 145.0,
-          "expected": 0.0,
-        }
-      ].forEach((testData) {
-        test('gets the shared value', () {
-          var item = Item(
-            name: "foo",
-            value: testData["value"] as double,
-            partition: 3,
-          );
+      var item = Item(name: "foo", value: 145.0, partition: 3);
 
-          item.assignees = testData["decisions"] as List<AssigneeDecision>;
+      createPartitionedSharedValueTest(
+          {condition, partsList, value, expectedValue}) {
+        createSharedValueTest(
+          item,
+          condition: condition,
+          partsList: partsList,
+          value: value,
+          expectedValue: expectedValue,
+        );
+      }
 
-          expect(item.getSharedValueFor('1'), testData["expected"] as double);
-        });
-      });
+      createPartitionedSharedValueTest(
+        condition: "everybody accepted 1 part",
+        partsList: [1, 1, 1],
+        value: item.value,
+        expectedValue: item.value / 3,
+      );
+
+      createPartitionedSharedValueTest(
+        condition: "accepted 2 and everyone else undefined",
+        partsList: [2, null, null],
+        value: item.value,
+        expectedValue: item.value * 2 / 3,
+      );
+
+      createPartitionedSharedValueTest(
+        condition: "accepted all and everyone else denied",
+        partsList: [3, 0, 0],
+        value: item.value,
+        expectedValue: item.value,
+      );
+
+      createPartitionedSharedValueTest(
+        condition: "everyone denied",
+        partsList: [0, 0, 0],
+        value: item.value,
+        expectedValue: 0.0,
+      );
+
+      createPartitionedSharedValueTest(
+        condition: "undefined and everybody else did something else",
+        partsList: [null, 0, 2],
+        value: item.value,
+        expectedValue: 0.0,
+      );
     });
+  });
+}
+
+createSharedValueTest(
+  Item item, {
+  required String condition,
+  required List<int?> partsList,
+  required double value,
+  required double expectedValue,
+}) {
+  test('calculates the shared value when $condition', () {
+    item.assignees = partsList
+        .map((parts) => AssigneeDecision(
+              uid: partsList.indexOf(parts).toString(),
+              parts: parts,
+            ))
+        .toList();
+
+    expect(item.getSharedValueFor('0'), expectedValue);
   });
 }
