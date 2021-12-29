75b84eb28aaecfa5f56c406cb5f08d53fb93b188
ANDREYDEN <andrey2850@gmail.com>
2021-12-29 17:50:18 +0200

    Add empty acreen for owings list

--- lib/ui/views/group_home.dart
+++ lib/ui/views/group_home.dart
@@ -11,6 +11,7 @@ import 'package:statera/data/states/group_state.dart';
 import 'package:statera/ui/viewModels/authentication_vm.dart';
 import 'package:statera/ui/widgets/dialogs/ok_cancel_dialog.dart';
 import 'package:statera/ui/widgets/listItems/owing_list_item.dart';
+import 'package:statera/ui/widgets/list_empty.dart';
 import 'package:statera/ui/widgets/loader.dart';
 
 class GroupHome extends StatelessWidget {
@@ -33,40 +34,39 @@ class GroupHome extends StatelessWidget {
                 style: Theme.of(context).textTheme.subtitle1,
               ),
               BlocBuilder<GroupCubit, GroupState>(
-                builder: (context, groupState) {
-                  if (groupState is GroupLoadingState) {
-                    return Center(child: Loader());
-                  }
-
-                  if (groupState is GroupErrorState) {
-                    return Text(groupState.error.toString());
-                  }
-                  
-                  if (groupState is GroupLoadedState) {
-                    return TextButton(
-                      onPressed: () async {
-                        ClipboardData data = ClipboardData(
-                          text: groupState.group.code.toString(),
-                        );
-                        await Clipboard.setData(data);
-                      },
-                      child: Row(
-                        children: [
-                          Text(
-                            groupState.group.code.toString(),
-                            style: TextStyle(
-                              fontWeight: FontWeight.w900,
-                              fontSize: 16,
-                            ),
-                          ),
-                          Icon(Icons.copy),
-                        ],
-                      ),
-                    );
-                  }
-                  return Text('Something went wrong');
+                  builder: (context, groupState) {
+                if (groupState is GroupLoadingState) {
+                  return Center(child: Loader());
                 }
-              )
+
+                if (groupState is GroupErrorState) {
+                  return Text(groupState.error.toString());
+                }
+
+                if (groupState is GroupLoadedState) {
+                  return TextButton(
+                    onPressed: () async {
+                      ClipboardData data = ClipboardData(
+                        text: groupState.group.code.toString(),
+                      );
+                      await Clipboard.setData(data);
+                    },
+                    child: Row(
+                      children: [
+                        Text(
+                          groupState.group.code.toString(),
+                          style: TextStyle(
+                            fontWeight: FontWeight.w900,
+                            fontSize: 16,
+                          ),
+                        ),
+                        Icon(Icons.copy),
+                      ],
+                    ),
+                  );
+                }
+                return Text('Something went wrong');
+              })
             ],
           ),
         ),
@@ -79,19 +79,21 @@ class GroupHome extends StatelessWidget {
         Flexible(
           child: StreamProvider<Map<Author, double>>(
             initialData: {},
-            create: (context) => GroupService.instance
-                .getOwingsForUserInGroup(authVm.user.uid, groupCubit.loadedState.group.id),
+            create: (context) => GroupService.instance.getOwingsForUserInGroup(
+                authVm.user.uid, groupCubit.loadedState.group.id),
             child: Consumer<Map<Author, double>>(
-              builder: (_, owings, __) => ListView.builder(
-                itemCount: owings.length,
-                itemBuilder: (context, index) {
-                  var payer = owings.keys.elementAt(index);
-                  return OwingListItem(
-                    member: payer,
-                    owing: owings[payer]!,
-                  );
-                },
-              ),
+              builder: (_, owings, __) => owings.isEmpty
+                  ? ListEmpty(text: 'Nobody here accept you...')
+                  : ListView.builder(
+                      itemCount: owings.length,
+                      itemBuilder: (context, index) {
+                        var payer = owings.keys.elementAt(index);
+                        return OwingListItem(
+                          member: payer,
+                          owing: owings[payer]!,
+                        );
+                      },
+                    ),
             ),
           ),
         ),
