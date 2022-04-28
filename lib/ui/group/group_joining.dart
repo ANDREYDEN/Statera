e78845fff2f7712bad53d7e50d072314d0814b5c
ANDREYDEN <andrey2850@gmail.com>
2022-04-27 21:32:18 -0400

    Add edge cases

--- lib/ui/group/group_joining.dart
+++ lib/ui/group/group_joining.dart
@@ -1,7 +1,13 @@
 import 'package:flutter/material.dart';
+import 'package:provider/src/provider.dart';
+import 'package:statera/business_logic/auth/auth_bloc.dart';
+import 'package:statera/data/services/services.dart';
+import 'package:statera/ui/groups/group_list.dart';
 import 'package:statera/ui/widgets/page_scaffold.dart';
+import 'package:statera/ui/widgets/protected_elevated_button.dart';
 
 import 'group_builder.dart';
+import 'group_page.dart';
 
 class GroupJoining extends StatelessWidget {
   static String route = '/join';
@@ -17,6 +23,12 @@ class GroupJoining extends StatelessWidget {
       return PageScaffold(child: Text('Invalid group reference'));
     }
 
+    var user = context.select((AuthBloc authBloc) => authBloc.state.user);
+
+    if (user == null) {
+      return PageScaffold(child: Text("Unauthorized"));
+    }
+
     return PageScaffold(
       child: Center(
         child: Container(
@@ -25,48 +37,83 @@ class GroupJoining extends StatelessWidget {
             color: Colors.grey[200],
             child: Padding(
               padding: const EdgeInsets.all(20),
-              child: GroupBuilder(builder: (context, group) {
-                return Column(
-                  mainAxisSize: MainAxisSize.min,
-                  children: [
-                    Text(
-                      'You are about to join',
-                      style: Theme.of(context).textTheme.headline6,
-                    ),
-                    Text(
-                      group.name,
-                      style: Theme.of(context)
-                          .textTheme
-                          .headline6!
-                          .copyWith(fontWeight: FontWeight.bold),
-                      textAlign: TextAlign.center,
-                    ),
-                    SizedBox(height: 30),
-                    Row(
+              child: GroupBuilder(
+                builder: (context, group) {
+                  String? error;
+
+                  if (group.userExists(user.uid)) {
+                    error = 'You are already a member of this group';
+                  } else if (group.code != code) {
+                    error = 'Invalid group invitation. Make sure you have copied it properly.';
+                  }
+
+                  if (error != null) {
+                    return Column(
+                      mainAxisSize: MainAxisSize.min,
                       children: [
-                        Expanded(
-                          child: ElevatedButton(
-                            onPressed: () {},
-                            child: Text('Join'),
+                        Text(error),
+                        SizedBox(height: 30),
+                        ElevatedButton(
+                          onPressed: () => Navigator.pushReplacementNamed(
+                            context,
+                            GroupList.route,
                           ),
-                        ),
-                        SizedBox(width: 10),
-                        Expanded(
-                          child: ElevatedButton(
-                            onPressed: () {},
-                            child: Text('Cancel'),
-                            style: ButtonStyle(
-                              backgroundColor: MaterialStateProperty.all(
-                                Theme.of(context).errorColor,
+                          child: Text('Back'),
+                        )
+                      ],
+                    );
+                  }
+
+                  return Column(
+                    mainAxisSize: MainAxisSize.min,
+                    children: [
+                      Text(
+                        'You are about to join',
+                        style: Theme.of(context).textTheme.headline6,
+                      ),
+                      Text(
+                        group.name,
+                        style: Theme.of(context)
+                            .textTheme
+                            .headline6!
+                            .copyWith(fontWeight: FontWeight.bold),
+                        textAlign: TextAlign.center,
+                      ),
+                      SizedBox(height: 30),
+                      Row(
+                        children: [
+                          Expanded(
+                            child: ProtectedElevatedButton(
+                              onPressed: () async {
+                                group.addUser(user);
+                                await GroupService.instance.saveGroup(group);
+                                Navigator.pushReplacementNamed(
+                                    context, '${GroupPage.route}${group.id}');
+                              },
+                              child: Text('Join'),
+                            ),
+                          ),
+                          SizedBox(width: 10),
+                          Expanded(
+                            child: ElevatedButton(
+                              onPressed: () {
+                                Navigator.pushReplacementNamed(
+                                    context, GroupList.route);
+                              },
+                              child: Text('Cancel'),
+                              style: ButtonStyle(
+                                backgroundColor: MaterialStateProperty.all(
+                                  Theme.of(context).errorColor,
+                                ),
                               ),
                             ),
                           ),
-                        ),
-                      ],
-                    )
-                  ],
-                );
-              }),
+                        ],
+                      )
+                    ],
+                  );
+                },
+              ),
             ),
           ),
         ),
