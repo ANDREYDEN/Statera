daf58f6cff361571aba2ce13c940845e0b974f20
ANDREYDEN <andrey2850@gmail.com>
2022-05-05 18:27:45 -0400

    Remove extra package

--- lib/ui/group/group_page.dart
+++ lib/ui/group/group_page.dart
@@ -1,5 +1,4 @@
 import 'package:firebase_auth/firebase_auth.dart';
-import 'package:firebase_dynamic_links/firebase_dynamic_links.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:statera/business_logic/auth/auth_bloc.dart';
