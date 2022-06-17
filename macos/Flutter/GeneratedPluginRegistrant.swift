324f6612f1a5830b71cea9d1c964010fb384c59d
Andrii Denysenko <a.denysenko@vehikl.com>
2022-06-17 11:22:40 -0600

    MacOS support (#90)
    * add ios config
    
    * Add permissions
    
    * Setup fastlane
    
    * Add fastlane settings
    
    * Add .gitignore files
    
    * WIP: app store submission
    
    * Update packages
    
    * WIP: add support page
    
    * Add links to the support page
    
    * Add spacing
    
    * Add navigation
    
    * Include support URL
    
    * Add sign in with Apple
    
    * Update Icons
    
    * Add MacOS support
    
    * Add app category field
    
    * Fix entiry addition changes
    
    * Clean up integration tests
    
    * Remove test
    
    Co-authored-by: Vehikl Team <ques@vehikl.com>

--- macos/Flutter/GeneratedPluginRegistrant.swift
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -10,7 +10,6 @@ import cloud_functions
 import desktop_webview_auth
 import firebase_auth
 import firebase_core
-import firebase_crashlytics
 import firebase_storage
 import share_plus_macos
 import sign_in_with_apple
@@ -22,7 +21,6 @@ func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   DesktopWebviewAuthPlugin.register(with: registry.registrar(forPlugin: "DesktopWebviewAuthPlugin"))
   FLTFirebaseAuthPlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseAuthPlugin"))
   FLTFirebaseCorePlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseCorePlugin"))
-  FLTFirebaseCrashlyticsPlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseCrashlyticsPlugin"))
   FLTFirebaseStoragePlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseStoragePlugin"))
   SharePlusMacosPlugin.register(with: registry.registrar(forPlugin: "SharePlusMacosPlugin"))
   SignInWithApplePlugin.register(with: registry.registrar(forPlugin: "SignInWithApplePlugin"))
