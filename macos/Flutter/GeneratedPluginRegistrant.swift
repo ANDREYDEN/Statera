29ccb9ce2adc30aa6ca54c906eb162fb5b5861f6
Andrii Denysenko <a.denysenko@vehikl.com>
2022-07-26 22:22:36 -0400

    Add Crashlytics & Improve SIgn In (#96)
    * Add crashlytics and sign in timeout
    
    * Resolve versions
    
    * Decrease sign in timeout
    
    * Add sign in improvements
    
    * Add display name input to settings
    
    * Add required error message
    
    * Update users in groups
    
    * Fix firebase auth version
    
    * Update deeplinking for iOS and macOS
    
    * Implement photo upload
    
    * Bump version
    
    * Update iOS build

--- macos/Flutter/GeneratedPluginRegistrant.swift
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -10,6 +10,7 @@ import cloud_functions
 import desktop_webview_auth
 import firebase_auth
 import firebase_core
+import firebase_crashlytics
 import firebase_storage
 import package_info_plus_macos
 import share_plus_macos
@@ -22,6 +23,7 @@ func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   DesktopWebviewAuthPlugin.register(with: registry.registrar(forPlugin: "DesktopWebviewAuthPlugin"))
   FLTFirebaseAuthPlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseAuthPlugin"))
   FLTFirebaseCorePlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseCorePlugin"))
+  FLTFirebaseCrashlyticsPlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseCrashlyticsPlugin"))
   FLTFirebaseStoragePlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseStoragePlugin"))
   FLTPackageInfoPlusPlugin.register(with: registry.registrar(forPlugin: "FLTPackageInfoPlusPlugin"))
   SharePlusMacosPlugin.register(with: registry.registrar(forPlugin: "SharePlusMacosPlugin"))
