15eeb94f9a3b52db917114d0f4b499c33dfaf365
Andrii Denysenko <a.denysenko@vehikl.com>
2022-08-18 22:17:10 -0400

    Add LCBO store (#119)
    * Add LCBO store
    
    * Add lcbo as store option
    
    * Update functions/src/functions/analyzeReceipt.ts
    
    * Update lib/ui/expense/dialogs/receipt_scan_dialog.dart
    
    * Fix type issue
    
    * Add semicolons
    
    * Fix normalizers
    
    * Add backwards compatibility
    
    * Add snapshot test for receipt analysis
    
    * Simplify mocks
    
    * Use jest snapshot
    
    * Add Walmart test

--- functions/src/transformators/normalizers.ts
+++ functions/src/transformators/normalizers.ts
@@ -1,4 +1,4 @@
-import { Product, WalmartProduct } from '../types/products'
+import { LCBOProduct, Product, WalmartProduct } from '../types/products'
 
 const CODE_REGEX = /\d{11,13}/
 const VALUE_REGEX = /\$?(\d+(\.|,)\d+)/
@@ -49,4 +49,50 @@ export function normalizeWalmartProducts(rows: string[][]): WalmartProduct[] {
 
     return product
   })
+}
+
+export function normalizeLCBOProducts(rows: string[][]): LCBOProduct[] {
+  const products: LCBOProduct[] = []
+  let currentProduct: LCBOProduct = {
+    name: '',
+    value: 0,
+    deposit: 0,
+    id: '',
+    volume: 0,
+    quantity: 0
+  }
+  const idVolumeDepositRegex = /(\d{8})\s*(\d{5})ML\s*DEP\s*(\d*\.\d{2})\s*ea/
+  const quantityValueRegex = /\(\s*(\d+)\s*@\s*(\d+\.\d{2})\s*\)/
+
+  const textRows = rows.map(row => row.join(' '))
+
+  for (const row of textRows) {
+    const idVolumeDepositMatcher = row.match(idVolumeDepositRegex)
+    if (idVolumeDepositMatcher) {
+      currentProduct.id = idVolumeDepositMatcher[1]
+      currentProduct.volume = +idVolumeDepositMatcher[2]
+      currentProduct.deposit = +idVolumeDepositMatcher[3]
+      continue
+    }
+
+    const quantityValueMatcher = row.match(quantityValueRegex)
+    if (quantityValueMatcher) {
+      currentProduct.quantity = +quantityValueMatcher[1]
+      currentProduct.value = +quantityValueMatcher[2]
+
+      products.push(currentProduct)
+      currentProduct = {
+        name: '',
+        value: 0,
+        deposit: 0,
+        id: '',
+        volume: 0,
+        quantity: 0
+      }
+      continue
+    }
+
+    currentProduct.name = row
+  }
+  return products
 }
