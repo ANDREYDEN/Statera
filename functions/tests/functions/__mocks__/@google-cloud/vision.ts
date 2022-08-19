6d07dc2c42dd72bc04153e7b83d33843202fbf2b
ANDREYDEN <andrey2850@gmail.com>
2022-08-18 21:35:58 -0400

    Simplify mocks

--- functions/tests/functions/__mocks__/@google-cloud/vision.ts
+++
@@ -1,9 +0,0 @@
-import lcboReceiptData from '../../../__stubs__/lcbo_receipt_data.json'
-
-export class ImageAnnotatorClient {
-  constructor() {}
-
-  async textDetection(url: string) {
-    return Promise.resolve(lcboReceiptData)
-  }
-}
--- functions/tests/functions/analyzeReceipt.test.ts
+++ functions/tests/functions/analyzeReceipt.test.ts
@@ -1,9 +1,17 @@
 import { analyzeReceipt } from "../../src/functions/analyzeReceipt"
 import lcboReceiptProducts from '../__stubs__/lcbo_receipt_result.json'
+import lcboReceiptData from '../__stubs__/lcbo_receipt_data.json'
 
+const textDetection = jest.fn()
+jest.mock('@google-cloud/vision', () => ({
+  ImageAnnotatorClient: jest.fn().mockImplementation(function() {
+    return { textDetection }
+  })
+}))
 
 describe('analyzeReceipt', () => {
   it('can analyze LCBO receipt', async () => {
+    textDetection.mockResolvedValue(lcboReceiptData)
     const products = await analyzeReceipt('https://example.com', false, 'lcbo')
 
     expect(products).toEqual(lcboReceiptProducts)
