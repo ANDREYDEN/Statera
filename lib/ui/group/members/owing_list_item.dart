dd8609d2611d8779fc7b325299c600c882d98e17
ANDREYDEN <andrey2850@gmail.com>
2022-09-23 23:12:41 -0400

    Highlight debt red if larger than threshold

--- lib/ui/group/members/owing_list_item.dart
+++ lib/ui/group/members/owing_list_item.dart
@@ -1,9 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
-import 'package:statera/business_logic/group/group_cubit.dart';
 import 'package:statera/business_logic/layout/layout_state.dart';
 import 'package:statera/business_logic/owing/owing_cubit.dart';
 import 'package:statera/data/models/author.dart';
+import 'package:statera/ui/group/group_builder.dart';
 import 'package:statera/ui/group/group_page.dart';
 import 'package:statera/ui/payments/payment_list_page.dart';
 import 'package:statera/ui/widgets/author_avatar.dart';
@@ -21,28 +21,42 @@ class OwingListItem extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    final groupCubit = context.read<GroupCubit>();
     final owingCubit = context.read<OwingCubit>();
     final isWide = context.read<LayoutState>().isWide;
 
-    return InkWell(
-      onTap: () => isWide
-          ? owingCubit.load(member.uid)
-          : Navigator.of(context).pushNamed(
-              "${GroupPage.route}/${groupCubit.loadedState.group.id}${PaymentListPage.route}/${member.uid}",
+    return GroupBuilder(
+      builder: (context, group) {
+        final paymentPageRoute =
+            '${GroupPage.route}/${group.id}${PaymentListPage.route}/${member.uid}';
+        final owingColor = this.owing >= group.debtThreshold
+            ? Theme.of(context).errorColor
+            : null;
+
+        return InkWell(
+          onTap: () => isWide
+              ? owingCubit.load(member.uid)
+              : Navigator.of(context).pushNamed(paymentPageRoute),
+          child: Container(
+            padding: const EdgeInsets.all(8.0),
+            decoration: BoxDecoration(
+              border: Border(
+                bottom: BorderSide(color: Color.fromARGB(255, 204, 204, 204)),
+              ),
             ),
-      child: Container(
-        padding: const EdgeInsets.all(8.0),
-        decoration: BoxDecoration(
-            border: Border(
-                bottom: BorderSide(color: Color.fromARGB(255, 204, 204, 204)))),
-        child: Row(
-          children: [
-            Expanded(child: AuthorAvatar(author: this.member, withName: true)),
-            PriceText(value: this.owing, textStyle: TextStyle(fontSize: 18)),
-          ],
-        ),
-      ),
+            child: Row(
+              children: [
+                Expanded(
+                  child: AuthorAvatar(author: this.member, withName: true),
+                ),
+                PriceText(
+                  value: this.owing,
+                  textStyle: TextStyle(fontSize: 18, color: owingColor),
+                ),
+              ],
+            ),
+          ),
+        );
+      },
     );
   }
 }
