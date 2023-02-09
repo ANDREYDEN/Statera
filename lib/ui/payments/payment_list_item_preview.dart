add55516152da7114eb6a4a69579520f541dfbbb
Andrii Denysenko <andrey2850@gmail.com>
2023-02-09 13:37:24 -0500

    Dynamic theme (#169)
    * Add dynamic theme
    
    * Switch danger button styles
    
    * Add monochrome icon
    
    * Use navigation rail
    
    * Switch expense item text color
    
    * Change expense card colors
    
    * Extract custom theme builder

--- lib/ui/payments/payment_list_item_preview.dart
+++ lib/ui/payments/payment_list_item_preview.dart
@@ -4,6 +4,7 @@ import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:mockito/mockito.dart';
 import 'package:statera/business_logic/auth/auth_bloc.dart';
 import 'package:statera/business_logic/group/group_cubit.dart';
+import 'package:statera/custom_theme_builder.dart';
 import 'package:statera/data/models/custom_user.dart';
 import 'package:statera/data/models/group.dart';
 import 'package:statera/data/models/payment.dart';
@@ -12,7 +13,6 @@ import 'package:statera/data/services/expense_service.mocks.dart';
 import 'package:statera/data/services/group_service.mocks.dart';
 import 'package:statera/data/services/user_repository.mocks.dart';
 import 'package:statera/ui/payments/payment_list_item.dart';
-import 'package:statera/utils/theme.dart';
 
 class MockUser extends Mock implements User {
   String get uid =>
@@ -48,100 +48,104 @@ class ListCover extends StatelessWidget {
     when(user.uid).thenReturn('a');
     when(authService.currentUser).thenReturn(user);
 
-    return MaterialApp(
-      theme: theme,
-      darkTheme: darkTheme,
-      themeMode: ThemeMode.system,
-      home: MultiBlocProvider(
-        providers: [
-          BlocProvider(
-            create: (_) => GroupCubit(
-              MockGroupService(),
-              MockExpenseService(),
-              MockUserRepository(),
-            )..loadGroup(Group(
-                name: 'Example',
-                members: [user1, user2],
-              )),
-          ),
-          BlocProvider(
-            create: (_) => AuthBloc(authService),
-          )
-        ],
-        child: Scaffold(
-          body: ListView(
-            children: [
-              PaymentListItem(
-                payment: Payment(
-                  groupId: 'asd',
-                  payerId: 'a',
-                  receiverId: 'b',
-                  value: 123,
-                  timeCreated: DateTime.now(),
-                ),
+    return CustomThemeBuilder(
+      builder: (lightTheme, darkTheme) {
+        return MaterialApp(
+          theme: lightTheme,
+          darkTheme: darkTheme,
+          themeMode: ThemeMode.system,
+          home: MultiBlocProvider(
+            providers: [
+              BlocProvider(
+                create: (_) => GroupCubit(
+                  MockGroupService(),
+                  MockExpenseService(),
+                  MockUserRepository(),
+                )..loadGroup(Group(
+                    name: 'Example',
+                    members: [user1, user2],
+                  )),
               ),
-              PaymentListItem(
-                payment: Payment(
-                  groupId: 'asd',
-                  payerId: 'a',
-                  receiverId: 'b',
-                  value: 123,
-                  relatedExpense: PaymentExpenseInfo(
-                    id: 'dummy_expense',
-                    name: 'Some Expense',
-                  ),
-                ),
-              ),
-              PaymentListItem(
-                payment: Payment(
-                  groupId: 'asd',
-                  payerId: 'a',
-                  receiverId: 'b',
-                  value: 123,
-                  reason: 'There was a malfunction in the system',
-                ),
-              ),
-              PaymentListItem(
-                payment: Payment(
-                  groupId: 'asd',
-                  payerId: 'a',
-                  receiverId: 'b',
-                  value: 123,
-                  reason:
-                      'This is a very long and unneeded explanation that there was a malfunction in the system',
-                ),
-              ),
-              PaymentListItem(
-                payment: Payment(
-                  groupId: 'asd',
-                  payerId: 'a',
-                  receiverId: 'b',
-                  value: 123,
-                  oldPayerBalance: 33,
-                ),
-              ),
-              PaymentListItem(
-                payment: Payment(
-                  groupId: 'asd',
-                  payerId: 'a',
-                  receiverId: 'b',
-                  value: 50,
-                  oldPayerBalance: 10,
-                ),
-              ),
-              PaymentListItem(
-                payment: Payment(
-                  groupId: 'asd',
-                  payerId: 'b',
-                  receiverId: 'a',
-                  value: 30,
-                  oldPayerBalance: -40,
-                ),
+              BlocProvider(
+                create: (_) => AuthBloc(authService),
               )
             ],
+            child: Scaffold(
+              body: ListView(
+                children: [
+                  PaymentListItem(
+                    payment: Payment(
+                      groupId: 'asd',
+                      payerId: 'a',
+                      receiverId: 'b',
+                      value: 123,
+                      timeCreated: DateTime.now(),
+                    ),
+                  ),
+                  PaymentListItem(
+                    payment: Payment(
+                      groupId: 'asd',
+                      payerId: 'a',
+                      receiverId: 'b',
+                      value: 123,
+                      relatedExpense: PaymentExpenseInfo(
+                        id: 'dummy_expense',
+                        name: 'Some Expense',
+                      ),
+                    ),
+                  ),
+                  PaymentListItem(
+                    payment: Payment(
+                      groupId: 'asd',
+                      payerId: 'a',
+                      receiverId: 'b',
+                      value: 123,
+                      reason: 'There was a malfunction in the system',
+                    ),
+                  ),
+                  PaymentListItem(
+                    payment: Payment(
+                      groupId: 'asd',
+                      payerId: 'a',
+                      receiverId: 'b',
+                      value: 123,
+                      reason:
+                          'This is a very long and unneeded explanation that there was a malfunction in the system',
+                    ),
+                  ),
+                  PaymentListItem(
+                    payment: Payment(
+                      groupId: 'asd',
+                      payerId: 'a',
+                      receiverId: 'b',
+                      value: 123,
+                      oldPayerBalance: 33,
+                    ),
+                  ),
+                  PaymentListItem(
+                    payment: Payment(
+                      groupId: 'asd',
+                      payerId: 'a',
+                      receiverId: 'b',
+                      value: 50,
+                      oldPayerBalance: 10,
+                    ),
+                  ),
+                  PaymentListItem(
+                    payment: Payment(
+                      groupId: 'asd',
+                      payerId: 'b',
+                      receiverId: 'a',
+                      value: 30,
+                      oldPayerBalance: -40,
+                    ),
+                  )
+                ],
+              ),
+            ),
           ),
-        ),
-      ),
+        );
+      },
     );
   }
 }
