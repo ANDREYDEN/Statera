ff248675907a9f14a899541192ff4e972f83ed66
adenysenko <a.denysenko@vehikl.com>
2023-02-14 13:31:51 -0500

    Add Expenses List test

---
+++ test/ui/group/expenses/expense_list_test.dart
@@ -0,0 +1,101 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:mockito/mockito.dart';
+import 'package:provider/provider.dart';
+import 'package:statera/business_logic/auth/auth_bloc.dart';
+import 'package:statera/business_logic/expense/expense_bloc.dart';
+import 'package:statera/business_logic/expenses/expenses_cubit.dart';
+import 'package:statera/business_logic/group/group_cubit.dart';
+import 'package:statera/business_logic/layout/layout_state.dart';
+import 'package:statera/data/models/models.dart';
+import 'package:statera/data/services/auth_service.mocks.dart';
+import 'package:statera/data/services/expense_service.mocks.dart';
+import 'package:statera/data/services/group_service.mocks.dart';
+import 'package:statera/data/services/user_repository.mocks.dart';
+import 'package:statera/ui/group/expenses/expense_list.dart';
+
+class MockUser extends Mock implements User {
+  String get uid =>
+      super.noSuchMethod(Invocation.getter(#uid), returnValue: 'foo');
+}
+
+void main() {
+  group('Expense List', () {
+    final groupService = MockGroupService();
+    final expenseService = MockExpenseService();
+    final userRepository = MockUserRepository();
+    final authService = MockAuthService();
+    final currentUser = MockUser();
+    final currentUserId = 'foo';
+    final otherUserId = 'bar';
+    final groupId = 'group_foo';
+    final testGroup = Group(
+      id: groupId,
+      name: 'Group Foo',
+      members: [
+        CustomUser(uid: currentUserId, name: 'Foo'),
+        CustomUser(uid: otherUserId, name: 'Bar')
+      ],
+    );
+
+    final expenses = [
+      Expense(name: 'E1', authorUid: currentUserId),
+      Expense(name: 'E2', authorUid: otherUserId)
+    ];
+
+    when(expenseService.listenForRelatedExpenses(any, any))
+        .thenAnswer((_) => Stream.fromIterable([expenses]));
+    when(groupService.groupStream(any))
+        .thenAnswer((_) => Stream.fromIterable([testGroup]));
+    when(currentUser.uid).thenReturn(currentUserId);
+    when(authService.currentUser).thenAnswer((_) => currentUser);
+
+    testWidgets('shows all group expenses', (WidgetTester tester) async {
+      await tester.pumpWidget(
+        MultiProvider(
+          providers: [
+            Provider<LayoutState>(create: (_) => LayoutState.narrow()),
+            BlocProvider(create: (context) => ExpenseBloc(expenseService)),
+            BlocProvider(
+                create: (context) =>
+                    GroupCubit(groupService, expenseService, userRepository)
+                      ..load(groupId)),
+            BlocProvider(
+              create: (context) => ExpensesCubit(expenseService, groupService)
+                ..load(currentUserId, groupId),
+            ),
+            BlocProvider(create: (context) => AuthBloc(authService))
+          ],
+          child: MaterialApp(home: Scaffold(body: ExpenseList())),
+        ),
+      );
+      await tester.pumpAndSettle();
+
+      for (var expense in expenses) {
+        expect(find.text(expense.name), findsOneWidget);
+      }
+    });
+
+    group('filtering', () {
+      testWidgets(
+        'can select finalized expenses',
+        (WidgetTester tester) async {},
+        skip: true,
+      );
+
+      testWidgets(
+        'can select pending expenses',
+        (WidgetTester tester) async {},
+        skip: true,
+      );
+
+      testWidgets(
+        'can select not marked expenses',
+        (WidgetTester tester) async {},
+        skip: true,
+      );
+    });
+  });
+}
