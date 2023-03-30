49bfd21ad997bcf14ac34dee4fe9ea71de959156
adenysenko <a.denysenko@vehikl.com>
2023-03-29 21:29:35 -0400

    Configure update banner to call firebase functions

--- lib/ui/groups/update_banner.dart
+++ lib/ui/groups/update_banner.dart
@@ -1,17 +1,41 @@
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
+import 'package:package_info_plus/package_info_plus.dart';
+import 'package:statera/data/services/callables.dart';
 
 class UpdateBanner extends StatelessWidget {
   const UpdateBanner({Key? key}) : super(key: key);
 
+  Future<String?> _getNewerVersionIfExists() async {
+    String? newerVersion;
+    if (defaultTargetPlatform == TargetPlatform.android) {
+      newerVersion = await Callables.getLatestAndroidVersion();
+    }
+
+    if (defaultTargetPlatform == TargetPlatform.iOS) {
+      newerVersion = await Callables.getLatestIOSVersion();
+    }
+
+    final currentVersion = await PackageInfo.fromPlatform();
+    final currentVersionNumber = currentVersion.version;
+    if (newerVersion != currentVersionNumber) {
+      return newerVersion;
+    }
+
+    return null;
+  }
+
   @override
   Widget build(BuildContext context) {
-    return FutureBuilder(
-      future: Future.delayed(Duration(seconds: 1)),
+    return FutureBuilder<String?>(
+      future: _getNewerVersionIfExists(),
       builder: (context, snapshot) {
-        if (snapshot.connectionState != ConnectionState.done)
+        if (snapshot.connectionState != ConnectionState.done ||
+            snapshot.data == null) {
           return SizedBox.shrink();
+        }
 
-        var newerVersion = '1.0.0';
+        var newerVersion = snapshot.data!;
 
         return Container(
           color: Colors.green,
