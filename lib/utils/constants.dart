f77b3893530dc5d1630cb6e69ccd645e5cf770e7
adenysenko <a.denysenko@vehikl.com>
2023-06-09 21:28:58 +0100

    Remove update banner debug refresh frequency

--- lib/utils/constants.dart
+++ lib/utils/constants.dart
@@ -25,4 +25,6 @@ const kSignInWithGoogleMessages = {
 
 const kRequiredValidationMessage = "Can't be empty";
 
+const kUpdateBannerRefreshFrequency = Duration(hours: 1);
+
 const kNotificationsReminderCooldown = Duration(days: 2);
