bdba1bab0c323a70e605f0b91ed1e5df5fa7bf6c
adenysenko <a.denysenko@vehikl.com>
2023-08-28 21:10:02 -0400

    WIP: add manual tests

--- functions/tests/functions/analyzeReceipt.test.ts
+++ functions/tests/functions/analyzeReceipt.test.ts
@@ -1,6 +1,8 @@
 import { analyzeReceipt } from '../../src/functions/analyzeReceipt'
 import lcboReceiptData from '../__stubs__/lcbo_receipt_data.json'
 import walmartReceiptData from '../__stubs__/walmart_receipt_data.json'
+import walmartReceiptMediumData from '../__stubs__/walmart_receipt_medium/data.json'
+import walmartReceiptMediumExpected from '../__stubs__/walmart_receipt_medium/expected.json'
 
 const textDetection = jest.fn()
 jest.mock('@google-cloud/vision', () => ({
@@ -15,6 +17,13 @@ describe('analyzeReceipt', () => {
     const products = await analyzeReceipt('https://example.com', false, 'walmart')
 
     expect(products).toMatchSnapshot()
+  })
+
+  it('can analyze a medium Walmart receipt', async () => {
+    textDetection.mockResolvedValue(walmartReceiptMediumData)
+    const products = await analyzeReceipt('https://example.com', false, 'walmart')
+
+    expect(products).toEqual(walmartReceiptMediumExpected)
   })
 
   it('can analyze LCBO receipt', async () => {
