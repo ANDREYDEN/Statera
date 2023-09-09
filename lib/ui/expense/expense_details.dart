6cf201217e11ec1481e1117d69c71a2fe4534eca
adenysenko <a.denysenko@vehikl.com>
2023-09-09 17:30:18 -0400

    Make key text selectable

--- lib/ui/expense/expense_details.dart
+++ lib/ui/expense/expense_details.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:statera/business_logic/auth/auth_bloc.dart';
 import 'package:statera/business_logic/expense/expense_bloc.dart';
