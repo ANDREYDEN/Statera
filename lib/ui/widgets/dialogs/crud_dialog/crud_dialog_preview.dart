8290572e131fae17060aa91559c3fbe9f6a21c69
Andrii Denysenko <andrey2850@gmail.com>
2024-10-07 23:27:45 -0400

    Item Creation Styling fixes (#281)
    Fix overflow issue

--- lib/ui/widgets/dialogs/crud_dialog/crud_dialog_preview.dart
+++ lib/ui/widgets/dialogs/crud_dialog/crud_dialog_preview.dart
@@ -14,59 +14,24 @@ main() {
         builder: (context) {
           return Column(
             children: [
-              ElevatedButton(
-                child: Text('Simple dialog'),
-                onPressed: () {
-                  showCRUDDialog(
-                    context,
-                    CRUDDialog(
-                      title: 'Edit Expense',
-                      fields: [
-                        FieldData(
-                          id: 'expense_name',
-                          label: 'Expense name',
-                          validators: [FieldData.requiredValidator],
-                          initialData: '',
-                        )
-                      ],
-                      onSubmit: (values) async {},
-                    ),
-                  );
-                },
+              CRUDDialogButton(
+                label: 'Simple',
+                crudDialog: simpleDialog,
               ),
-              ElevatedButton(
-                child: Text('Segmented dialog'),
-                onPressed: () {
-                  showCRUDDialog(
-                    context,
-                    CRUDDialog.segmented(
-                      title: 'Edit Expense',
-                      segments: [
-                        ButtonSegment(value: 'first', label: Text('First')),
-                        ButtonSegment(value: 'second', label: Text('Second'))
-                      ],
-                      fieldsMap: {
-                        'first': [
-                          FieldData(
-                            id: 'first_field',
-                            label: 'First Field',
-                            validators: [FieldData.requiredValidator],
-                            initialData: '',
-                          )
-                        ],
-                        'second': [
-                          FieldData(
-                            id: 'second_field',
-                            label: 'Second Field',
-                            validators: [FieldData.requiredValidator],
-                            initialData: '',
-                          )
-                        ]
-                      },
-                      onSubmit: (values) async {},
-                    ),
-                  );
-                },
+              SizedBox(height: 10),
+              CRUDDialogButton(
+                label: 'Simple with many fields',
+                crudDialog: simpleDialogWithManyFields,
+              ),
+              SizedBox(height: 10),
+              CRUDDialogButton(
+                label: 'Segmented',
+                crudDialog: segmentedDialog,
+              ),
+              SizedBox(height: 10),
+              CRUDDialogButton(
+                label: 'Advanced + Add another',
+                crudDialog: advancedAddAnotherDialog,
               ),
             ],
           );
@@ -76,12 +41,105 @@ main() {
   );
 }
 
-Future<void> showCRUDDialog(BuildContext context, Widget dialog) {
-  return showDialog(
-    context: context,
-    builder: (_) => Provider.value(
-      value: context.read<LayoutState>(),
-      child: dialog,
+final simpleDialog = CRUDDialog(
+  title: 'Edit Expense',
+  fields: [
+    FieldData(
+      id: 'expense_name',
+      label: 'Expense name',
+      validators: [FieldData.requiredValidator],
+      initialData: '',
+    )
+  ],
+  onSubmit: (values) async {},
+);
+
+final simpleDialogWithManyFields = CRUDDialog(
+  title: 'Edit Expense',
+  fields: List.generate(
+    5,
+    (i) => FieldData(
+      id: 'field$i',
+      label: 'Field $i',
+      initialData: '',
     ),
-  );
+  ),
+  onSubmit: (values) async {},
+);
+
+final segmentedDialog = CRUDDialog.segmented(
+  title: 'Edit Expense',
+  segments: [
+    ButtonSegment(value: 'first', label: Text('First')),
+    ButtonSegment(value: 'second', label: Text('Second'))
+  ],
+  fieldsMap: {
+    'first': [
+      FieldData(
+        id: 'first_field',
+        label: 'First Field',
+        validators: [FieldData.requiredValidator],
+        initialData: '',
+      )
+    ],
+    'second': [
+      FieldData(
+        id: 'second_field',
+        label: 'Second Field',
+        validators: [FieldData.requiredValidator],
+        initialData: '',
+      )
+    ]
+  },
+  onSubmit: (values) async {},
+);
+
+final advancedAddAnotherDialog = CRUDDialog(
+  title: 'Edit Expense',
+  fields: [
+    FieldData(
+      id: 'first_field',
+      label: 'First Field',
+      validators: [FieldData.requiredValidator],
+      initialData: '',
+    ),
+    FieldData(
+      id: 'second_field',
+      label: 'Second Field',
+      validators: [FieldData.requiredValidator],
+      initialData: '',
+    ),
+    FieldData(
+      id: 'advanced_field',
+      label: 'Advanced Field',
+      validators: [FieldData.requiredValidator],
+      initialData: '',
+      isAdvanced: true,
+    ),
+  ],
+  allowAddAnother: true,
+  onSubmit: (values) async {},
+);
+
+class CRUDDialogButton extends StatelessWidget {
+  final String label;
+  final CRUDDialog crudDialog;
+  const CRUDDialogButton(
+      {super.key, required this.crudDialog, required this.label});
+
+  @override
+  Widget build(BuildContext context) {
+    return ElevatedButton(
+      child: Text(label),
+      onPressed: () {
+        showDialog(
+          context: context,
+          builder: (_) => Provider.value(
+            value: context.read<LayoutState>(),
+            child: crudDialog,
+          ),
+        );
+      },
+    );
+  }
 }
