fa5c2d258e3a21b80a836959309ef0f84a3be26a
SergeyPutilov <sergey98putilov@gmail.com>
2025-01-10 23:45:08 -0500

    added a elipsis to the owing list

--- lib/ui/group/members/owing_list_item.dart
+++ lib/ui/group/members/owing_list_item.dart
@@ -1,12 +1,15 @@
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
 import 'package:provider/provider.dart';
+import 'package:statera/business_logic/auth/auth_bloc.dart';
 import 'package:statera/business_logic/layout/layout_state.dart';
 import 'package:statera/business_logic/owing/owing_cubit.dart';
 import 'package:statera/data/models/custom_user.dart';
 import 'package:statera/ui/group/group_builder.dart';
+import 'package:statera/ui/group/members/actions/kick_member_action.dart';
 import 'package:statera/ui/group/members/new_payments_badge.dart';
 import 'package:statera/ui/payments/payment_list_page.dart';
+import 'package:statera/ui/widgets/buttons/actions_button.dart';
 import 'package:statera/ui/widgets/price_text.dart';
 import 'package:statera/ui/widgets/user_avatar.dart';
 
@@ -24,6 +27,7 @@ class OwingListItem extends StatelessWidget {
   Widget build(BuildContext context) {
     final owingCubit = context.watch<OwingCubit>();
     final isWide = context.read<LayoutState>().isWide;
+    final uid = context.select<AuthBloc, String>((authBloc) => authBloc.uid);
 
     String? selectedMemberUid = null;
 
@@ -36,45 +40,52 @@ class OwingListItem extends StatelessWidget {
         final owingColor = this.owing >= group.debtThreshold
             ? Theme.of(context).colorScheme.error
             : null;
-        final isAdmin = group.admin.uid == this.member.uid;
+        final isCurrentMemberAdmin = group.admin.uid == this.member.uid;
+        final isGroupMember = group.isAdmin(uid);
 
         return MouseRegion(
           cursor: SystemMouseCursors.click,
           child: ListTile(
-            onTap: () => isWide
-                ? owingCubit.select(member.uid)
-                : context.goNamed(
-                    PaymentListPage.name,
-                    pathParameters: {
-                      'groupId': group.id!,
-                      'memberId': member.uid
-                    },
+              onTap: () => isWide
+                  ? owingCubit.select(member.uid)
+                  : context.goNamed(
+                      PaymentListPage.name,
+                      pathParameters: {
+                        'groupId': group.id!,
+                        'memberId': member.uid
+                      },
+                    ),
+              selected: selectedMemberUid == member.uid,
+              selectedTileColor:
+                  Theme.of(context).colorScheme.primary.withAlpha(50),
+              title: Row(
+                children: [
+                  Expanded(
+                    child: UserAvatar(
+                      author: this.member,
+                      withName: true,
+                      withIcon: isCurrentMemberAdmin,
+                      icon: isCurrentMemberAdmin ? Icons.star : null,
+                      iconColor: isCurrentMemberAdmin ? Colors.yellow : null,
+                      iconBackgroudColor:
+                          isCurrentMemberAdmin ? Colors.black : null,
+                    ),
                   ),
-            selected: selectedMemberUid == member.uid,
-            selectedTileColor:
-                Theme.of(context).colorScheme.primary.withAlpha(50),
-            title: Row(
-              children: [
-                Expanded(
-                  child: UserAvatar(
-                    author: this.member,
-                    withName: true,
-                    withIcon: isAdmin,
-                    icon: isAdmin ? Icons.star : null,
-                    iconColor: isAdmin ? Colors.yellow : null,
-                    iconBackgroudColor: isAdmin ? Colors.black : null,
+                  NewPaymentsBadge(
+                    memberId: member.uid,
+                    child: PriceText(
+                      value: this.owing,
+                      textStyle: TextStyle(fontSize: 18, color: owingColor),
+                    ),
                   ),
-                ),
-                NewPaymentsBadge(
-                  memberId: member.uid,
-                  child: PriceText(
-                    value: this.owing,
-                    textStyle: TextStyle(fontSize: 18, color: owingColor),
-                  ),
-                ),
-              ],
-            ),
-          ),
+                ],
+              ),
+              trailing: isGroupMember
+                  ? ActionsButton(
+                      tooltip: 'Whatsss up?',
+                      actions: [KickMemberAction()],
+                    )
+                  : null),
         );
       },
     );
