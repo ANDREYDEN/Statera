d3caad06297fc4633902e23a453d91804184b94f
ANDREYDEN <andrey2850@gmail.com>
2025-03-18 23:09:25 -0400

    Refactor transactions

--- lib/data/services/coordination_repository.dart
+++ lib/data/services/coordination_repository.dart
@@ -3,17 +3,14 @@ import 'package:statera/data/models/models.dart';
 import 'package:statera/data/services/firestore.dart';
 
 class CoordinationRepository extends Firestore {
-  CoordinationRepository(FirebaseFirestore firestoreInstance) : super(firestoreInstance);
+  CoordinationRepository(FirebaseFirestore firestoreInstance)
+      : super(firestoreInstance);
 
   Future<void> finalizeExpense(String groupId, String expenseId) async {
     await firestore.runTransaction((transaction) async {
-      final expenseDocRef = expensesCollection.doc(expenseId);
-      final expenseDoc = await transaction.get(expenseDocRef);
-      final expense = Expense.fromSnapshot(expenseDoc);
-
-      final groupDocRef = groupsCollection.doc(groupId);
-      final groupDoc = await transaction.get(groupDocRef);
-      final group = Group.fromFirestore(groupDoc.data() as Map<String, dynamic>, id: groupDoc.id);
+      final (expense, expenseDocRef) =
+          await _getExpense(transaction, expenseId);
+      final (group, groupDocRef) = await _getGroup(transaction, groupId);
 
       expense.finalizedDate = Timestamp.now().toDate();
       await transaction.set(expenseDocRef, expense.toFirestore());
@@ -22,15 +19,16 @@ class CoordinationRepository extends Firestore {
       final payments = expense.assigneeUids
           .where((assigneeUid) => assigneeUid != expense.authorUid)
           .map(
-            (assigneeUid) {
-              return Payment.fromFinalizedExpense(
-                expense: expense,
-                receiverId: assigneeUid,
-                oldAuthorBalance: group.balance[expense.authorUid]?[assigneeUid],
-              );
-            },
+        (assigneeUid) {
+          return Payment.fromFinalizedExpense(
+            expense: expense,
+            receiverId: assigneeUid,
+            oldAuthorBalance: group.balance[expense.authorUid]?[assigneeUid],
           );
-      await Future.wait(payments.map((payment) => paymentsCollection.add(payment.toFirestore())));
+        },
+      );
+      await Future.wait(payments
+          .map((payment) => paymentsCollection.add(payment.toFirestore())));
       for (final payment in payments) {
         group.payOffBalance(payment: payment);
       }
@@ -40,13 +38,9 @@ class CoordinationRepository extends Firestore {
 
   Future<void> revertExpense(String groupId, String expenseId) async {
     await firestore.runTransaction((transaction) async {
-      final expenseDocRef = expensesCollection.doc(expenseId);
-      final expenseDoc = await transaction.get(expenseDocRef);
-      final expense = Expense.fromSnapshot(expenseDoc);
-
-      final groupDocRef = groupsCollection.doc(groupId);
-      final groupDoc = await transaction.get(groupDocRef);
-      final group = Group.fromFirestore(groupDoc.data() as Map<String, dynamic>, id: groupDoc.id);
+      final (expense, expenseDocRef) =
+          await _getExpense(transaction, expenseId);
+      final (group, groupDocRef) = await _getGroup(transaction, groupId);
 
       expense.finalizedDate = null;
       await transaction.set(expenseDocRef, expense.toFirestore());
@@ -62,11 +56,39 @@ class CoordinationRepository extends Firestore {
             ),
           );
 
-      await Future.wait(payments.map((payment) => paymentsCollection.add(payment.toFirestore())));
+      await Future.wait(
+        payments
+            .map((payment) => paymentsCollection.add(payment.toFirestore())),
+      );
       for (final payment in payments) {
         group.payOffBalance(payment: payment);
       }
       await transaction.set(groupDocRef, group.toFirestore());
     });
+  }
+
+  Future<(Expense, DocumentReference)> _getExpense(
+    Transaction transaction,
+    String expenseId,
+  ) async {
+    final expenseDocRef = expensesCollection.doc(expenseId);
+    final expenseDoc = await transaction.get(expenseDocRef);
+    final expense = Expense.fromSnapshot(expenseDoc);
+
+    return (expense, expenseDocRef);
+  }
+
+  Future<(Group, DocumentReference)> _getGroup(
+    Transaction transaction,
+    String groupId,
+  ) async {
+    final groupDocRef = groupsCollection.doc(groupId);
+    final groupDoc = await transaction.get(groupDocRef);
+    final group = Group.fromFirestore(
+      groupDoc.data() as Map<String, dynamic>,
+      id: groupDoc.id,
+    );
+
+    return (group, groupDocRef);
   }
 }
