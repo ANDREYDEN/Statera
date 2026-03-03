2bff06a482f4218f413b62820458279306ba2aae
ANDREYDEN <andrey2850@gmail.com>
2026-03-02 21:42:08 -0500

    Remove Walmart name improvements

--- functions/tests/utils.test.ts
+++
@@ -1,8 +0,0 @@
-import { stripSku } from '../src/utils'
-
-describe('Utils', () => {
-  it('strips SKU of leading 0s', () => {
-    const cleanSku = stripSku('00012340123')
-    expect(cleanSku).toEqual('12340123')
-  })
-})
