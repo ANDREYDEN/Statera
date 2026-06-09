41ed0cc0fe2df80abeb3cc44d2f38ae48f78a293
ANDREYDEN <andrey2850@gmail.com>
2026-06-08 22:51:25 -0400

    Add action test

--- test/ui/expense/expense_page_test.dart
+++ test/ui/expense/expense_page_test.dart
@@ -1,5 +1,8 @@
+import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
+import 'package:mockito/mockito.dart';
 import 'package:statera/data/models/models.dart';
+import 'package:statera/ui/expense/expense_details.dart';
 import 'package:statera/ui/expense/expense_page.dart';
 
 import '../../helpers.dart';
@@ -23,6 +26,50 @@ void main() {
       expect(find.text('Milk'), findsOneWidget);
       expect(find.text('Eggs'), findsOneWidget);
     });
+
+    group('Expense Actions', () {
+      group('Add tax to all items', () {
+        testWidgets('applies tax to all items', (tester) async {
+          final expense = Expense(
+            name: 'Grocery Run',
+            authorUid: defaultCurrentUserId,
+          );
+          expense.settings.tax = 0.13;
+          final item1 = SimpleItem(
+            name: 'Milk',
+            value: 10.0,
+            assigneeUids: [defaultCurrentUserId],
+          );
+          item1.setAssigneeDecision(defaultCurrentUserId, 1);
+          final item2 = SimpleItem(
+            name: 'Eggs',
+            value: 5.0,
+            assigneeUids: [defaultCurrentUserId],
+          );
+          item2.setAssigneeDecision(defaultCurrentUserId, 1);
+          expense.items.add(item1);
+          expense.items.add(item2);
+
+          await pumpExpensePage(tester, expense: expense);
+          await tester.pumpAndSettle();
+
+          expectExepenseTotalIs('\$15.00');
+
+          await tester.tap(find.byIcon(Icons.more_vert));
+          await tester.pumpAndSettle();
+
+          await tester.tap(find.text('Add tax to all items'));
+          await tester.pumpAndSettle();
+
+          await tester.tap(find.text('Yes'));
+          await tester.pumpAndSettle();
+
+          expect(find.textContaining('+ tax'), findsExactly(2));
+          expect(find.textContaining('Tax ('), findsOneWidget);
+          expectExepenseTotalIs('\$16.95');
+        });
+      });
+    });
   });
 }
 
@@ -30,10 +77,27 @@ Future<void> pumpExpensePage(
   WidgetTester tester, {
   required Expense expense,
 }) async {
+  when(
+    defaultExpenseService.expenseStream(any),
+  ).thenAnswer((_) => Stream.value(expense));
+
   await customPump(
     ExpensePage(),
     tester,
     group: defaultGroup,
     selectedExpense: expense,
   );
+}
+
+void expectExepenseTotalIs(String expectedTotal) {
+  final totalWidget = find.ancestor(
+    of: find.text('Your Total'),
+    matching: find.byType(FooterEntry),
+  );
+
+  final totalTextWidget = find.descendant(
+    of: totalWidget,
+    matching: find.text(expectedTotal),
+  );
+  expect(totalTextWidget, findsOneWidget);
 }
